2814d2cc8ea9604528741399ec879f2c86cde035
casperversteeg <casper.versteeg@outlook.com>
2019-07-04 18:32:57 -0400

    Improving file readability

---
+++ src/contentloader.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      elmnt.removeAttribute("include-html");
+      $(elmnt).load(file);
+      return;
+    };
+  };
+});
