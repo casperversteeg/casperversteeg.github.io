1848d860e317c4a6d8b3f45a668d49f7e38f01d4
casperversteeg <casper.versteeg@outlook.com>
2019-08-05 11:29:26 -0400

    Fixed menu button active on page

--- src/contentloader.js
+++ src/contentloader.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var z, i, elmnt, file;
+  var z, i, elmnt, file, page_id;
   /*loop through a collection of all HTML elements:*/
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
@@ -7,8 +7,13 @@ $(document).ready(function() {
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("include-html");
     if (file) {
+      page_id = elmnt.getAttribute("page-id");
       elmnt.removeAttribute("include-html");
-      $(elmnt).load(file);
+      $(elmnt).load(file, function() {
+        if (page_id) {
+          $("#" + page_id).addClass("active");
+        };
+      });
       return;
     };
   };
